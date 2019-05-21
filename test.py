b406b82e155876c15c8ee460b7839e4b9cb95761
litoomet <liisa.toomet@gmail.com>
2019-05-20 22:04:51 -0700

    first test

---
+++ test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+adjective = input("please enter an adjective: ")
+noun = input("please enter a noun: ")
+verb = input("please enter a verb: ")
+animal = input("please enter a second noun: ")
+print("your madlib: ")
+print("The", adjective, noun, verb, "over the lazy green", animal)
